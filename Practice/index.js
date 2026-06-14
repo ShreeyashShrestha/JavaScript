d0eaf21f761474f0abadea820959ec04e75dca0e
ShreeyashShrestha <shreeyashofficialpc1@gmail.com>
2026-06-14 19:14:33 +0545

    06/14

--- Practice/index.js
+++ Practice/index.js
@@ -1943,4 +1943,27 @@
 
 // console.log(childern);
 
+// ADD & CHANGE HTML
 
+const newListItem = document.createElement("li");
+
+newListItem.textContent = "coconut";
+newListItem.id = "coconut";
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "green";
+
+//document.body.append(newListItem);
+//document.body.prepend(newListItem);
+document.getElementById("fruits").append(newListItem);
+//document.getElementById("fruits").prepend(newListItem);
+
+// const banana = document.getElementById("banana");
+// document.getElementById("fruits").insertBefore(newListItem, banana)
+
+// const orange = document.getElementById("orange");
+// document.getElementById("fruits").insertBefore(newListItem, orange);
+
+// const listItems = document.querySelectorAll("#fruits li");
+// document.getElementById("fruits").insertBefore(newListItem, listItems[3]);
+
+document.getElementById("fruits").removeChild(newListItem);
